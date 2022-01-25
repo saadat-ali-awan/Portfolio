9cecd277d2e8fb0c0bb3d8acfaaf72d845fda79e
JuanLP <juan.luis.palacios.p@gmail.com>
2022-01-25 15:53:32 -0500

    Add templating function for modal

--- js/main.js
+++ js/main.js
@@ -2,11 +2,25 @@ function menuToggle() {
   document.querySelector('#nav-menu').classList.toggle('toggle-mobile-menu');
 }
 
+function modalTemplate(obj) {
+  document.querySelector('.popUpScreen h1').textContent = obj.name;
+  document.querySelector('.popUpScreen p').textContent = obj.description
+  document.querySelector('.popUpScreen > img').src = obj['featured-image']
+  document.querySelector('.popUpScreen ul').innerHTML = '';
+  obj.technologies.forEach(x => {
+    const li = document.createElement('li');
+    li.textContent = x;
+    document.querySelector('.popUpScreen ul').appendChild(li)
+  });
+  document.querySelector('.popUpScreen .actions a:nth-child(1)').href = obj['live-version']
+  document.querySelector('.popUpScreen .actions a:nth-child(2)').href = obj['repo-link']
+}
+
 let projects = [
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'name': 'hello',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
@@ -20,8 +34,8 @@ let projects = [
   },
   {
     'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
@@ -35,8 +49,8 @@ let projects = [
   },
   {
     'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
@@ -50,8 +64,8 @@ let projects = [
   },
   {
     'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
@@ -65,8 +79,8 @@ let projects = [
   },
   {
     'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
@@ -80,8 +94,8 @@ let projects = [
   },
   {
     'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    'featured-image': 'img/SnapshootPortfolioDesktop',
+    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    'featured-image': 'img/SnapshootPortfolioDesktop.png',
     'technologies': [
       'Codekit',
       'GitHub',
