cbfd6c96b37b32a323bb638d8003764d9bb5c25d
Saadat123456 <saadatlucky313@gmail.com>
2022-01-26 02:18:58 +0500

    Add Button Click Functionality For PopUp

--- js/main.js
+++ js/main.js
@@ -4,110 +4,111 @@ function menuToggle() {
 
 function modalTemplate(obj) {
   document.querySelector('.popUpScreen h1').textContent = obj.name;
-  document.querySelector('.popUpScreen p').textContent = obj.description
-  document.querySelector('.popUpScreen > img').src = obj['featured-image']
+  document.querySelector('.popUpScreen p').textContent = obj.description;
+  document.querySelector('.popUpScreen > img').src = obj['featured-image'];
   document.querySelector('.popUpScreen ul').innerHTML = '';
-  obj.technologies.forEach(x => {
+  obj.technologies.forEach((x) => {
     const li = document.createElement('li');
     li.textContent = x;
-    document.querySelector('.popUpScreen ul').appendChild(li)
+    document.querySelector('.popUpScreen ul').appendChild(li);
   });
-  document.querySelector('.popUpScreen .actions a:nth-child(1)').href = obj['live-version']
-  document.querySelector('.popUpScreen .actions a:nth-child(2)').href = obj['repo-link']
+  document.querySelector('.popUpScreen .actions a:nth-child(1)').href = obj['live-version'];
+  document.querySelector('.popUpScreen .actions a:nth-child(2)').href = obj['repo-link'];
+  document.querySelector('#popUpModal').style.display = 'block';
 }
 
-let projects = [
+const projects = [
   {
-    'name': 'hello',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
   {
-    'name': 'Multi-Post Stories Gain+Glory',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     'featured-image': 'img/SnapshootPortfolioDesktop.png',
-    'technologies': [
+    technologies: [
       'Codekit',
       'GitHub',
       'Javasript',
       'Bootstrap',
       'Terminal',
-      'Codepen'
+      'Codepen',
     ],
-    'live-version': "https://saadat123456.github.io/Portfolio/",
+    'live-version': 'https://saadat123456.github.io/Portfolio/',
     'repo-link': 'https://github.com/Saadat123456/Portfolio'
   },
-]
+];
 
 window.addEventListener('load', () => {
   document.querySelector('#menu-btn').addEventListener('click', () => {
@@ -116,5 +117,17 @@ window.addEventListener('load', () => {
 
   document.querySelector('#nav-menu').addEventListener('click', () => {
     menuToggle();
+  });
+
+  const popUpButtons = document.querySelectorAll('button[data-number]');
+  popUpButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const val = parseInt(btn.dataset.number, 10);
+      modalTemplate(projects[val]);
+    });
+  });
+
+  document.querySelector('.close-popup-btn').addEventListener('click', () => {
+    document.querySelector('#popUpModal').style.display = 'none';
   });
 });
