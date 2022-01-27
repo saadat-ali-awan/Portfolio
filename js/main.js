fea6cdc533ed7f509d9a433d9eda8bb3090ee301
Saadat123456 <saadatlucky313@gmail.com>
2022-01-27 22:58:01 +0500

    Add Error Message

--- js/main.js
+++ js/main.js
@@ -10,10 +10,18 @@ window.addEventListener('load', () => {
   document.querySelector('#nav-menu').addEventListener('click', () => {
     menuToggle();
   });
+
   const form = document.getElementById('contact-me-form');
+
   form.addEventListener('submit', (event) => {
-      if(form.email.value !== form.email.value.toLowerCase()){
-          event.preventDefault();
-      }
-  })
+    if (document.querySelector('small')) form.removeChild(document.querySelector('small'));
+    if (form.email.value !== form.email.value.toLowerCase()) {
+      event.preventDefault();
+      const listItem = document.createElement('li');
+      const errorMessage = document.createElement('small');
+      errorMessage.innerText = 'Email Contains Capital Letters';
+      listItem.append(errorMessage);
+      form.querySelector('ul').append(listItem);
+    }
+  });
 });
